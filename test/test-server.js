4ea3a17f12cb2f97f44258de0aa03db23abb42ce
Evan Thompson <evanbrowningthompson@gmail.com>
2017-12-30 16:22:28 -0500

    added tests for recipes CRUD requests

--- test/test-server.js
+++ test/test-server.js
@@ -13,9 +13,8 @@ const should = chai.should();
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
-
-describe('Shopping List', function() {
-
+describe('Shopping-List', function() {
+  
   // Before our tests run, we activate the server. Our `runServer`
   // function returns a promise, and we return the that promise by
   // doing `return runServer`. If we didn't return a promise here,
@@ -34,10 +33,6 @@ describe('Shopping List', function() {
     return closeServer();
   });
 
-  // test strategy:
-  //   1. make request to `/shopping-list`
-  //   2. inspect response object and prove has right code and have
-  //   right keys in response object.
   it('should list items on GET', function() {
     // for Mocha tests, when we're dealing with asynchronous operations,
     // we must either return a Promise object or else call a `done` callback
@@ -135,6 +130,81 @@ describe('Shopping List', function() {
       })
       .then(function(res) {
         res.should.have.status(204);
+      });
+  });
+});
+
+describe('Recipes', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+  
+  it('should return a JSON response for GET requests', function() {
+    (1).should.equal(1);
+    // it should return JSON data
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        res.should.be.json;
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.above(0);
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+    });
+  });   
+
+  it('should add an item on POST', function() {
+    const newRecipe = {name: 'curry', ingredients: ['veggies', 'sauce', 'spices']};
+    // it should return JSON data and a 201 status code
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function(res) {
+        res.should.be.json;
+        res.should.have.status(201);
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.id.should.not.be.null;
+        res.body.should.deep.equal(Object.assign(newRecipe, {id: res.body.id}))
+      });
+  });
+
+  it('should update an item on PUT', function() {
+    const updateData = {name: 'thing', ingredients: ['some', 'things']};
+    // it should return a status code of 204 after update
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+
+        return chai.request(app)
+          .put(`/recipes/${ updateData.id }`)
+          .send(updateData); 
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+
+  it('should delete an item on DELETE', function() {
+    // it should return a status code of 204 after deletion
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${ res.body[0].id }`)
+      })
+      .then(function(res) {
+        res.should.have.status(204)
       });
   });
 });
